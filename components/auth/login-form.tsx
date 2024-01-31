6c86c36f54209e1ef483b7e8c42ec357466fabc2
xapis-21 <xapisofficial@gmail.com>
2024-01-31 22:44:21 +0300

    commit

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,15 +1,101 @@
+"use client";
+
+import * as z from "zod";
+
+import { useTransition } from "react";
+
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { LoginSchema } from "@/schemas";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+
 import { CardWrapper } from "./card-wrapper";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import { FormError } from "../form-error";
+import { FormSuccess } from "../form-success";
+import { login } from "@/actions/login";
 
 export const LoginForm = () => {
+  const [isPending, startTransition] = useTransition();
+
+  const form = useForm<z.infer<typeof LoginSchema>>({
+    resolver: zodResolver(LoginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof LoginSchema>) => {
+    startTransition(() => {
+      login(values);
+    });
+  };
+
   return (
     <CardWrapper
       headerLabel={"Welcome back"}
       backButtonLabel={"Don't have an account"}
       backButtonHref={"/auth/register"}
       showSocial
-      
     >
-      login-form
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+          <div className="space-y-4">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      disabled={isPending}
+                      placeholder="johndoe@example.com"
+                      type="email"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      placeholder="******"
+                      type="password"
+                      disabled={isPending}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <FormError message="" />
+          <FormSuccess message="" />
+          <Button type="submit" className="w-full" disabled={isPending}>
+            Login
+          </Button>
+        </form>
+      </Form>
     </CardWrapper>
   );
 };
--- components/auth/social.tsx
+++ components/auth/social.tsx
@@ -7,10 +7,20 @@ interface SocialProps {}
 export const Social = () => {
   return (
     <div className="flex items-center w-full gap-x-2 ">
-      <Button className="w-full" size={"lg"} variant={"outline"} onClick={() => {}}>
+      <Button
+        className="w-full"
+        size={"lg"}
+        variant={"outline"}
+        onClick={() => {}}
+      >
         <FcGoogle className="h-5 w-5" />
       </Button>
-      <Button className="w-full" size={"lg"} variant={"outline"} onClick={() => {}}>
+      <Button
+        className="w-full"
+        size={"lg"}
+        variant={"outline"}
+        onClick={() => {}}
+      >
         <FaGithub className="h-5 w-5" />
       </Button>
     </div>
