a59aa0296689da5ba8e88ec961b0579754c13020
xapis-21 <xapisofficial@gmail.com>
2024-02-03 09:33:47 +0300

    get user

--- actions/register.ts
+++ actions/register.ts
@@ -1,14 +1,40 @@
-"use server"
+"use server";
 
-import { RegisterSchema } from "@/schemas"
-import * as z from "zod"
+import { RegisterSchema } from "@/schemas";
+import * as z from "zod";
+import bcrypt from "bcrypt";
+import { db } from "@/lib/db";
 
-export const register = async (values:  z.infer<typeof RegisterSchema>) =>{
-    const validatedFields = RegisterSchema.safeParse(values);
+export const register = async (values: z.infer<typeof RegisterSchema>) => {
+  const validatedFields = RegisterSchema.safeParse(values);
 
-    if(!validatedFields.success){
-        return { error: "Invalid fields!"};
-    }
+  if (!validatedFields.success) {
+    return { error: "Invalid fields!" };
+  }
 
-    return {success: "Email sent!"}
-}
+  const { email, password, name } = validatedFields.data;
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await db.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (existingUser) {
+    return { error: "Email already in use!" };
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  //TODO: Send verification token email
+  
+  return { success: "User Created successfully" };
+};
