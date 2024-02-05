abece904dad4a6931b71c067b21d3e1ff620cabf
xapis-21 <xapisofficial@gmail.com>
2024-02-05 21:30:14 +0300

    auth

--- auth.config.ts
+++ auth.config.ts
@@ -1,31 +1,41 @@
 import Credentials from "next-auth/providers/credentials";
+import Github from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
+
 import type { NextAuthConfig } from "next-auth";
 
 import { LoginSchema } from "@/schemas";
 import { getUserByEmail } from "./data/user";
-import bcrypt from "bcryptjs"
+import bcrypt from "bcryptjs";
 
 export default {
-  providers: [Credentials({
-    async authorize(credentials){
+  providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    Github({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
+    Credentials({
+      async authorize(credentials) {
         const validatedFields = LoginSchema.safeParse(credentials);
 
-        if(validatedFields.success){
-            const {email, password} = validatedFields.data;
+        if (validatedFields.success) {
+          const { email, password } = validatedFields.data;
 
-            const user = await getUserByEmail(email);
+          const user = await getUserByEmail(email);
 
-            if(!user || !user.password) return null;
+          if (!user || !user.password) return null;
 
-            const passwordsMatch = await bcrypt.compare(
-                password,
-                user.password
-            );
+          const passwordsMatch = await bcrypt.compare(password, user.password);
 
-            if(passwordsMatch) return user;
+          if (passwordsMatch) return user;
         }
 
         return null;
-    }
-  })],
+      },
+    }),
+  ],
 } satisfies NextAuthConfig;
--- components/auth/social.tsx
+++ components/auth/social.tsx
@@ -2,16 +2,26 @@
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
 import { Button } from "../ui/button";
+import { signIn } from "next-auth/react";
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
+import google from "next-auth/providers/google";
 
 interface SocialProps {}
 export const Social = () => {
+
+  const onClick = (provider: "google" | "github") => {
+    signIn(provider, {
+      callbackUrl: DEFAULT_LOGIN_REDIRECT
+    })
+  }
+
   return (
     <div className="flex items-center w-full gap-x-2 ">
       <Button
         className="w-full"
         size={"lg"}
         variant={"outline"}
-        onClick={() => {}}
+        onClick={() => onClick("google")}
       >
         <FcGoogle className="h-5 w-5" />
       </Button>
@@ -19,7 +29,7 @@ export const Social = () => {
         className="w-full"
         size={"lg"}
         variant={"outline"}
-        onClick={() => {}}
+        onClick={() => onClick("github")}
       >
         <FaGithub className="h-5 w-5" />
       </Button>
