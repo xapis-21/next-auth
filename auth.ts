1c40e5884b39c7af1945deb48a75d6c18eb4ac79
xapis-21 <xapisofficial@gmail.com>
2024-02-05 18:20:28 +0300

    callbacks and adding default fields to tokens

--- auth.ts
+++ auth.ts
@@ -1,7 +1,21 @@
-import NextAuth from "next-auth";
+import NextAuth, {DefaultSession} from "next-auth";
 import authConfig from "@/auth.config";
 import {PrismaAdapter} from "@auth/prisma-adapter"
 import { db } from "@/lib/db";
+import { getUserById } from "@/data/user";
+
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      /** Role of the user
+       *
+       */
+      role: "ADMIN" | "USER";
+    } & DefaultSession["user"];
+  } 
+}
+
 
 export const {
   handlers: { GET, POST },
@@ -9,6 +23,30 @@ export const {
   signIn,
   signOut
 } = NextAuth({
+  callbacks:{
+    async session({token, session}){
+      if(token.sub && session.user){
+        session.user.id = token.sub;
+      }
+
+      if(token.role && session.user){
+        session.user.role = token.role as "ADMIN" | "USER"
+      }
+      
+      return session;
+    },
+    async jwt({token}){
+      if(!token.sub) return token;
+
+      const existingUser = await getUserById(token.sub);
+
+      if(!existingUser) return token;
+
+      token.role = existingUser.role;
+
+      return token;
+    }
+  },
     adapter: PrismaAdapter(db),
     session: {strategy:"jwt"},
   ...authConfig
