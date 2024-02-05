631781eef4981d756df685b431bd095cf627d09e
xapis-21 <xapisofficial@gmail.com>
2024-02-05 16:24:54 +0300

    middelware and login

--- actions/login.ts
+++ actions/login.ts
@@ -1,14 +1,37 @@
-"use server"
+"use server";
 
-import { LoginSchema } from "@/schemas"
-import * as z from "zod"
+import { LoginSchema } from "@/schemas";
+import { signIn } from "@/auth";
+import * as z from "zod";
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
+import { AuthError } from "next-auth";
 
-export const login = async (values:  z.infer<typeof LoginSchema>) =>{
-    const validatedFields = LoginSchema.safeParse(values);
+export const login = async (values: z.infer<typeof LoginSchema>) => {
+  const validatedFields = LoginSchema.safeParse(values);
 
-    if(!validatedFields.success){
-        return { error: "Invalid fields!"};
+  if (!validatedFields.success) {
+    return { error: "Invalid fields!" };
+  }
+
+  const { email, password } = validatedFields.data;
+
+  try {
+    await signIn("credentials", {
+      email,
+      password,
+      redirectTo: DEFAULT_LOGIN_REDIRECT,
+    });
+  } catch (error) {
+    if(error instanceof AuthError){
+        switch (error.type) {
+            case "CredentialsSignin":
+                return {error: "Invalid credentials!"}
+                break;
+        
+            default:
+                return {error: "Somethig went wrong"}
+        }
     }
-
-    return {success: "Email sent!"}
-}
+    throw error;
+  }
+};
